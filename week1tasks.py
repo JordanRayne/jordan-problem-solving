2c57c10ca71b5341863baca44f957fbdee11788d
JORDAN MALLORY <0mallj73@solent.ac.uk>
2026-02-24 12:45:36 +0000

    week1tasks

---
+++ week1tasks.py
@@ -0,0 +1,45 @@
+print("hello world!")
+
+age= 19
+
+print(f"I am {age} Years old")
+
+first_number = 8
+second_number = 7
+answer = first_number + second_number
+
+print(f"{first_number}+{second_number}={answer}")
+
+
+price = 2.25
+quantity = 5
+total_price = price * quantity
+
+print(f"You brought {quantity} things for £{price}. That costs £{total_price}.")
+
+
+
+
+name = input("what is your name?")
+
+height =input("How tall are you?")
+
+print(f" Hi {name} You are {height} tall")
+
+
+
+favorite_number= input("what is your favorite number?")
+first_initial = input("what is your first initial of your first name ?")
+last_initial = input("what is your first initial of your last  name?")
+
+
+
+print(f"Hey {first_initial}{last_initial}! Your favorite number is {favorite_number}")
+
+
+item = input("what did you buy")
+price = int(input("how much did it costs"))
+quantity =int(input("how much did you get"))
+
+total_price = price * quantity
+
