98dd9f7331e5898b337b180e4e55b1c760c01df8
JORDAN MALLORY <0mallj73@solent.ac.uk>
2026-02-27 15:44:58 +0000

    week2tasks

--- week2tasks.py
+++ week2tasks.py
@@ -1,6 +1,34 @@
-age=int(input("Please enter your age"))
+# age=int(input("Please enter your age"))
+# #
+# # if age<=0:
+# #     print("wow, people start using computer early")
+#
+#
+# if age==19:
+#     print("Hey! We are the same age!")
 
-if age<=0:
-    print("wow, people start using computer early")
+
+# month=int(input("what is the month"))
+# day=int(input("what day is it"))
+#
+# if day == 1 and month == 4:
+#     print("Its april fools day")
 
 
+# if age>=18:
+#     print("you are old enough")
+#
+# else:
+#     print("you are to young")
+
+speed=int(input("what is the speed"))
+speedlimit=int(input("what is the limit"))
+
+
+if  speed > speedlimit+10:
+    print("speed and ticket and possible court summons")
+elif speed>speedlimit:
+    print("you are going to fast")
+
+else:
+    print("you are driving the correct speed")
