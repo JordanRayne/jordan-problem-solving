bf55a03082daaf2532e290c61190c6db45f272c7
JORDAN MALLORY <0mallj73@solent.ac.uk>
2026-03-10 12:48:01 +0000

    week3tasks

--- week3tasks.py
+++ week3tasks.py
@@ -1,10 +1,31 @@
-def calculate_discounted_price(price, discount_percent):
-    discount=price/100*discount_percent
+# def calculate_discounted_price(price, discount_percent):
+#     discount=price/100*discount_percent
+#
+#     return price-discount
+#
+#
+#
+# print(calculate_discounted_price(150,35))
 
-    return price-discount
+
+
+def grade_converter(grade):
+    if grade >= 70:
+        return "first"
+    elif grade >= 60:
+        return "upper second"
+    elif grade >= 50:
+        return "lower second"
+    elif grade >= 40:
+        return "Third"
+    return "refer"
 
 
 
-print(calculate_discounted_price(150,35))
+
+
+
+
+
 
 
